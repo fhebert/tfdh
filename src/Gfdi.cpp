d722260100460b7aa9c055d73afddc04aa95052c
Francois Hebert <fmh33@cornell.edu>
2014-10-05 19:15:07 -0400

    Gfdi: Use ++x increment in loops for consistency

--- src/Gfdi.cpp
+++ src/Gfdi.cpp
@@ -71,7 +71,7 @@ double gfdi(const GFDI order, const double chi, const double tau) {
 
   if (chi <= 0.6) {
     double value = 0;
-    for (int i=1; i<=5; i++) {
+    for (int i=1; i<=5; ++i) {
       value += c[k][i-1] * sqrt(1 + khi[k][i-1]*tau/2) /
         (exp(-khi[k][i-1]) + exp(-chi));
     }
@@ -79,7 +79,7 @@ double gfdi(const GFDI order, const double chi, const double tau) {
   }
   else if (chi < 14.0) {
     double value = 0;
-    for (int i=1; i<=5; i++) {
+    for (int i=1; i<=5; ++i) {
       value += h[i-1] * pow(x[i-1], k) * pow(chi, k+3./2)
         * sqrt(1 + chi*x[i-1]*tau/2) / (1 + exp(chi*(x[i-1] - 1)))
         + v[i-1] * pow(xi[i-1] + chi, k+1./2) * sqrt(1 + (xi[i-1] + chi)*tau/2);
