d856517a87050b192ed332e84e6440f52db861ca
Francois Hebert <fmh33@cornell.edu>
2014-12-22 15:17:18 -0800

    Add TFDH functions

---
+++ src/TfdhFunctions.cpp
@@ -0,0 +1,76 @@
+
+#include "TfdhFunctions.h"
+
+#include "GslWrappers.h"
+#include "PlasmaFunctions.h"
+#include "PlasmaState.h"
+#include "RadialFunction.h"
+
+#include <cmath>
+
+#include <iostream>
+
+namespace {
+
+  class FermiDiracDistribution : public GslFunction {
+    const double xi;
+    const PlasmaState p;
+    public:
+    FermiDiracDistribution(const double xi, const PlasmaState& p)
+      : xi(xi), p(p) {}
+    double f(const double x) const {
+      const double val = (1 + p.tau*x) * sqrt(x + p.tau * x*x/2.0) / (1.0 + exp(x-p.chi-xi));
+      //std::cout << " FermiDirac(xi=" << xi << ") computed at x=" << x << ", got val=" << val << std::endl;
+      //std::cout << "   x-chi-xi = " << x-p.chi-xi << ",  exp(x-chi-xi) = " << exp(x-p.chi-xi) << std::endl;
+      return val;
+    }
+  };
+
+  double boundFermiDiracIntegral(const double xi, const PlasmaState& p) {
+    FermiDiracDistribution fd(xi, p);
+    const double eps = 1.e-6;
+    return gslQuadratureAG(fd, 0, xi, eps, eps);
+  }
+
+}
+
+double TFDH::neBound(const double phi, const PlasmaState& p)
+{
+  const double xi = phi/p.kt;
+  // TODO: check these conditions!
+  if (xi < 0) {
+    return 0;
+  } else if (xi > 1.e4*(1+fabs(p.chi))) {
+    return Plasma::ne(xi, p); // xi very large => xi+chi ~ xi
+  } else {
+    return boundFermiDiracIntegral(xi, p);
+  }
+}
+
+RadialFunction TFDH::neBound(const RadialFunction& tfdh, const PlasmaState& p)
+{
+  /*
+  const int place = 1;
+  const double xi = tfdh.data[place]/tfdh.radii[place]/p.kt;
+  const FermiDiracDistribution fd(xi,p);
+  const int len = 1000;
+  std::vector<double> xs(len), fs(len);
+  for (size_t i=0; i<len; ++i) {
+    const double x = i/(len-1.0) * xi;
+    xs[i] = x;
+    fs[i] = fd.f(x);
+  }
+  const RadialFunction test(xs, fs);
+  writeToFile(test, "test.dat");
+  return test;
+  */
+  std::vector<double> nebs(tfdh.data.size());
+  for (size_t i=0; i<tfdh.radii.size(); ++i) {
+    //std::cout << "computing neB..." << std::endl;
+    nebs[i] = neBound(tfdh.data[i]/tfdh.radii[i], p);
+    //std::cout << "  success" << std::endl;
+  }
+  return RadialFunction(tfdh.radii, nebs);
+}
+
+
---
+++ src/TfdhFunctions.h
@@ -0,0 +1,16 @@
+
+#ifndef TFDH_TFDH_FUNCTIONS_H
+#define TFDH_TFDH_FUNCTIONS_H
+
+struct PlasmaState;
+struct RadialFunction;
+
+namespace TFDH {
+
+  double neBound(double phi, const PlasmaState& p);
+  RadialFunction neBound(const RadialFunction& tfdh, const PlasmaState& p);
+
+}
+
+
+#endif // TFDH_TFDH_FUNCTIONS_H
