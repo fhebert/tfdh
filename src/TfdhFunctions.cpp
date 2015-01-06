e0db7c549c4b42520b2f9c7aa771087ef5bf0c68
Francois Hebert <fmh33@cornell.edu>
2015-01-06 03:08:31 -0500

    TfdhFunctions: Remove debug code

--- src/TfdhFunctions.cpp
+++ src/TfdhFunctions.cpp
@@ -21,8 +21,6 @@ namespace {
       : xi(xi), p(p) {}
     double f(const double x) const {
       const double val = (1 + p.tau*x) * sqrt(x + p.tau * x*x/2.0) / (1.0 + exp(x-p.chi-xi));
-      //std::cout << " FermiDirac(xi=" << xi << ") computed at x=" << x << ", got val=" << val << std::endl;
-      //std::cout << "   x-chi-xi = " << x-p.chi-xi << ",  exp(x-chi-xi) = " << exp(x-p.chi-xi) << std::endl;
       return val;
     }
   };
@@ -52,26 +50,9 @@ double TFDH::neBound(const double phi, const PlasmaState& p)
 
 RadialFunction TFDH::neBound(const RadialFunction& tfdh, const PlasmaState& p)
 {
-  /*
-  const int place = 1;
-  const double xi = tfdh.data[place]/tfdh.radii[place]/p.kt;
-  const FermiDiracDistribution fd(xi,p);
-  const int len = 1000;
-  std::vector<double> xs(len), fs(len);
-  for (size_t i=0; i<len; ++i) {
-    const double x = i/(len-1.0) * xi;
-    xs[i] = x;
-    fs[i] = fd.f(x);
-  }
-  const RadialFunction test(xs, fs);
-  writeToFile(test, "test.dat");
-  return test;
-  */
   std::vector<double> nebs(tfdh.data.size());
   for (size_t i=0; i<tfdh.radii.size(); ++i) {
-    //std::cout << "computing neB..." << std::endl;
     nebs[i] = neBound(tfdh.data[i], p);
-    //std::cout << "  success" << std::endl;
   }
   return RadialFunction(tfdh.radii, nebs);
 }
